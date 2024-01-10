a3f0633d81316c198a23812bfcfc61748426f2ad
CristobalEspinosa <1889639@alu.murciaeduca.es>
2024-01-10 22:40:02 +0100

    terminado

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
+//slider
 import { getSlider } from 'simple-slider';
  
 getSlider();
+
+// cookies
+import cookieConsent from 'vanilla-cookieconsent';
+
+cookieConsent({
+  message: 'Este sitio web utiliza cookies para mejorar la experiencia del usuario.',
+  dismiss: 'Aceptar',
+  link: 'Leer más',
+  href: 'https://www.example.com/cookies',
+  container: document.body,
+  theme: 'light',
+  domain: 'example.com',
+  secure: true,
+  sameSite: 'strict'
+});
+
+// fecha
+import flatpickr from "flatpickr";
+
+const datepicker = flatpickr("#myDatepicker", {
+  enableTime: true,
+  dateFormat: "Y-m-d H:i",
+});
