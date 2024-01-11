91918bbcbc7ec88fdc5f71ec65b0766ffbd5bca3
CristobalEspinosa <1889639@alu.murciaeduca.es>
2024-01-11 18:50:14 +0100

    actualizacion de fontawesome

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,27 @@
 //slider
-import { getSlider } from 'simple-slider';
- 
+import { getSlider } from "simple-slider";
+
 getSlider();
 
 // cookies
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   window.cookieconsent.initialise({
-    "palette": {
-      "popup": {
-        "background": "#000"
+    palette: {
+      popup: {
+        background: "#000",
       },
-      "button": {
-        "background": "#f1d600"
-      }
+      button: {
+        background: "#f1d600",
+      },
     },
-    "content": {
-      "message": "Este sitio web utiliza cookies para garantizar que obtenga la mejor experiencia en nuestro sitio web.",
-      "dismiss": "Aceptar",
-      "link": "Leer más",
-      "href": "/cookies"
-    }
-  })
+    content: {
+      message:
+        "Este sitio web utiliza cookies para garantizar que obtenga la mejor experiencia en nuestro sitio web.",
+      dismiss: "Aceptar",
+      link: "Leer más",
+      href: "/cookies",
+    },
+  });
 });
 
 // fecha en identificarse.html
@@ -32,4 +33,4 @@ const datepicker = flatpickr("#myDatepicker", {
 });
 
 // galeria
-lightGallery(document.getElementById('lightgallery'));
+lightGallery(document.getElementById("lightgallery"));
